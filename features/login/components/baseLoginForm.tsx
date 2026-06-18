adbef2926515fa7ae4269a1a102263d7223d0b72
YarielZC <yarielzamoracueto@gmail.com>
2026-06-18 15:26:51 -0400

    feat(login): add BaseLoginForm component with username and password fields

---
+++ features/login/components/baseLoginForm.tsx
@@ -0,0 +1,94 @@
+'use client'
+
+import { Eye, EyeSlash, Lock, Person } from '@gravity-ui/icons'
+import { Button, Checkbox, FieldError, Form, Input, Label, TextField } from '@heroui/react'
+import Link from 'next/link'
+import { useState } from 'react'
+
+export default function BaseLoginForm() {
+  const [passwordVisible, setPasswordVisible] = useState(false)
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const data: Record<string, string> = {}
+
+    formData.forEach((value, key) => {
+      data[key] = value.toString()
+    })
+
+    alert(`Form submitted with: ${JSON.stringify(data, null, 2)}`)
+  }
+
+  return (
+    <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+      <TextField
+        isRequired
+        name="username"
+        type="text"
+        className="flex flex-col gap-2"
+        validate={(value) => {
+          if (!value) return 'Este campo no puede quedar vacío.'
+
+          return null
+        }}>
+        <Label className="font-normal">Nombre de usuario o Email</Label>
+        <div className="flex flex-col relative">
+          <Person className="absolute left-3 w-5 h-5 top-3 pointer-events-none text-[#71717A]" />
+          <Input
+            className="bg-[#f2f4f6] rounded-md font-light pl-10 py-3"
+            placeholder="Introduzca su usuario o email"
+          />
+        </div>
+
+        <FieldError />
+      </TextField>
+
+      <TextField
+        isRequired
+        name="password"
+        type="password"
+        className="flex flex-col gap-2"
+        validate={(value) => {
+          if (!value) return 'Este campo no puede quedar vacío.'
+
+          return null
+        }}>
+        <Label className="font-normal">Contraseña</Label>
+
+        <div className="flex flex-col relative">
+          <Lock className="absolute left-3 w-5 h-5 top-3 pointer-events-none text-[#71717A]" />
+
+          <Input
+            className="bg-[#f2f4f6] rounded-md font-light px-10 py-3"
+            placeholder="Introduza su contraseña"
+            type={passwordVisible ? 'text' : 'password'}
+          />
+
+          <button
+            type="button"
+            className="absolute right-3 top-3 flex items-center justify-center outline-none hover:text-foreground transition-colors cursor-pointer text-[#71717A]"
+            onClick={() => setPasswordVisible(!passwordVisible)}>
+            {passwordVisible ? <Eye className="w-5 h-5" /> : <EyeSlash className="w-5 h-5" />}
+          </button>
+        </div>
+        <FieldError />
+      </TextField>
+
+      <Checkbox variant="secondary">
+        <Checkbox.Content className="font-normal">
+          <Checkbox.Control>
+            <Checkbox.Indicator />
+          </Checkbox.Control>
+          Recuérdame
+        </Checkbox.Content>
+      </Checkbox>
+
+      <div className="flex gap-2 mt-4">
+        <Button className="rounded-md w-full py-6" type="submit">
+          Iniciar Sesión
+        </Button>
+      </div>
+    </Form>
+  )
+}

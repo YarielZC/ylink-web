772ca81cc36721eca5f598bf17c02aae0696adec
YarielZC <yarielzamoracueto@gmail.com>
2026-06-19 18:35:45 -0400

    feat(login): implement user authentication and redirect to dashboard on login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,24 @@
+'use client'
+
 import YlinkIcon from '@/components/ylinkIcon'
+import { useAuth } from '@/context/AuthContext'
 import LoginForm from '@/features/login/components/loginForm'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function Login() {
+  const { user, loading } = useAuth()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (user) {
+      router.replace('/dashboard')
+    }
+  }, [user, router])
+
+  if (user || loading) return null
+
   return (
     <section className="flex flex-col items-center gap-6 max-sm:gap-3.5">
       <div className="flex flex-col items-center gap-2">
